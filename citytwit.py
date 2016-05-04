1c5f14dc62822e9fa9f7665b0382824fbcb1a32a
Bryan Sills <bryan@bryansills.com>
2016-05-03 22:25:28 -0500

    add basic twit api

--- citytwit.py
+++ citytwit.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import datetime
+
 application = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+tweets = [
+    {
+        'author': 'me',
+        'text': 'yo',
+        'author_img': 'http://i.imgur.com/BA98QMi.jpg',
+        'timestamp': datetime.datetime.now()
+    }
+]
 
-if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+@application.route('/', methods=['GET'])
+def get_tweets():
+    return jsonify({'results': tweets})
+
+@application.route('/', methods=['POST'])
+def add_tweet():
+    tweet = {
+        'author': request.json['author'],
+        'text': request.json['text'],
+        'author_img': request.json['author_img'],
+        'timestamp': datetime.datetime.now()
+    }
+    tweets.append(tweet)
+    return jsonify({'results': tweets}), 201
+
+if __name__ == '__main__':
+    application.run(debug=True)
